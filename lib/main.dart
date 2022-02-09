88ffd50644db1b472657bb25a22ece280b30a100
RavinduDilshan <dilshan.ravidu@gmail.com>
2022-02-09 16:01:10 +0530

    app bar design added

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:facebookclone/config/palette.dart';
+import 'package:facebookclone/screens/screens.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Facebook Clone',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        scaffoldBackgroundColor: Palette.scaffold,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
